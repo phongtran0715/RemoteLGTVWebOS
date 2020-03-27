b223b902fde98065d367f222a56b57150590a603
phongtran0715 <phongtran0715@gmail.com>
2020-03-27 21:43:43 +0700

    Add GUI module

---
+++ LGTV/control.py
@@ -0,0 +1,191 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+from inspect import getargspec
+import json
+import os
+import sys
+from time import sleep
+from scan import LGTVScan
+from remote import LGTVRemote
+from auth import LGTVAuth
+
+
+search_config = [
+    "/etc/lgtv/config.json",
+    "~/.lgtv/config.json",
+    "/opt/venvs/lgtv/config/config.json"
+]
+
+def usage(error=None):
+    if error:
+        print ("Error: " + error)
+    print ("LGTV Controller")
+    print ("Author: Karl Lattimer <karl@qdh.org.uk>")
+    print ("Usage: lgtv <command> [parameter]\n")
+    print ("Available Commands:")
+
+    print ("  -i                    interactive mode")
+
+    print ("  scan")
+    print ("  auth <host>")
+
+    commands = LGTVRemote.getCommands()
+    for c in commands:
+        args = getargspec(LGTVRemote.__dict__[c])
+        if len(args.args) > 1:
+            a = ' <' + '> <'.join(args.args[1:-1]) + '>'
+            print ('  ' + c + a)
+        else:
+            print ('  ' + c)
+
+def parseargs(command, argv):
+    args = getargspec(LGTVRemote.__dict__[command])
+    args = args.args[1:-1]
+
+    if len(args) != len(argv):
+        raise Exception("Argument lengths do not match")
+
+    output = {}
+    for (i, a) in enumerate(args):
+        if argv[i].lower() == "true":
+            argv[i] = True
+        elif argv[i].lower() == "false":
+            argv[i] = False
+        try:
+            f = int(argv[i])
+            argv[i] = f
+        except:
+            try:
+                f = float(argv[i])
+                argv[i] = f
+            except:
+                pass
+        output[a] = argv[i]
+    return output
+
+
+def find_config():
+    w = None
+    for f in search_config:
+        f = os.path.expanduser(f)
+        f = os.path.abspath(f)
+        d = os.path.dirname(f)
+        if os.path.exists(d):
+            if os.access(d, os.W_OK):
+                w = f
+            if os.path.exists(f):
+                if os.access(f, os.W_OK):
+                    return f
+        elif os.access(os.path.dirname(d), os.W_OK):
+            os.makedirs(d)
+            w = f
+    if w is None:
+        print ("Cannot find suitable config path to write, create one in %s" % ' or '.join(search_config))
+        raise Exception("No config file")
+    return w
+
+def send_command(name, command, args, config):
+    if command == "on":
+        ws = LGTVRemote(name, **config[name])
+        ws.on()
+        return
+    else:
+        try:
+            args = parseargs(name, args)
+        except Exception as e:
+            usage(e.message)
+            return
+    try:
+        ws = LGTVRemote(name, **config[name])
+        ws.connect()
+        if command is not None:
+            ws.execute(command, args)
+        ws.run_forever()
+    except KeyboardInterrupt:
+        ws.close()
+
+def read_config_file(name, host):
+    data = None
+    config_file = name + "_" + host + ".json"
+    if os.path.isfile(config) == True:
+        with open(file) as f:
+            data = json.load(f)
+    return data
+
+def pair_device(name, host):
+    print("Start to authen device : %s - %s" % (name, host))
+    filename = name + "_" + host + ".json"
+    filename = os.path.join(workspace,filename)
+    ws = LGTVAuth(name, host)
+    ws.connect()
+    ws.run_forever()
+    sleep(1)
+    config = ws.serialise()
+    if filename is not None:
+        with open(filename, 'w') as f:
+            f.write(json.dumps(config))
+        print ("Wrote config file: " + filename)
+
+def read_devices_list(file):
+    devices = None
+    if os.path.isfile(file) == True:
+        with open(file) as f:
+            devices = json.load(f)
+    return devices
+
+def scan_devices(output_file):
+    results = LGTVScan()
+    if len(results) > 0:
+        print (json.dumps({
+            "result": "ok",
+            "count": len(results),
+            "list": results
+        }))
+        for it in results:
+            uuid = it['uuid']
+            address = it['address']
+            model = it['model']
+            #Save to json file
+            data = []
+            data.append({
+                'uuid': it['uuid'],
+                'model': it['model'],
+                'address': it['address'],
+            })
+        with open(outfile, 'w') as outfile:
+            json.dump(data, outfile)
+    else:
+        print (json.dumps({
+            "result": "failed",
+            "count": len(results)
+        }))
+        return
+
+def main():
+    devices_list = os.path.join(workspace,'devices.json')
+    # Scan all devices
+    scan_devices(devices_list)
+
+    # Read devices list
+    devices = read_devices_list(devices_list)
+
+    # Pair devices
+    for item in devices:
+        pair_device(item['uuid'], item['address'])
+
+    # Send command
+    for item in devices:
+        config =  read_config_file(item['uuid'], item['address'])
+        name = item['uuid']
+        command = "off"
+        args = []
+        send_command(name, command, args, data)
+        
+if __name__ == '__main__':
+    workspace = os.path.join(os.path.expanduser("~"),".lgtv")
+    if not os.path.exists(workspace):
+        os.makedirs(workspace)
+    print("workspace : %s" % workspace)
+    # main()
+    print("Bye")
+    input("Press Enter to continue...")
--- LGTV/main.py
+++ LGTV/main.py
@@ -1,191 +1,207 @@
-# -*- coding: utf-8 -*-
-from __future__ import print_function
-from inspect import getargspec
-import json
-import os
-import sys
-from time import sleep
-from scan import LGTVScan
-from remote import LGTVRemote
-from auth import LGTVAuth
+import tkinter as tk
+from tkinter import HORIZONTAL
+
+window = tk.Tk()
+
+window.title("Remote LG TVs")
+window.columnconfigure([0, 1], weight=1, minsize=75)
+window.rowconfigure([0, 1, 2, 3, 4, 5], weight=1, minsize=50)
+
+# adding image (remember image should be PNG and not JPG) 
+
+# adding image (remember image should be PNG and not JPG) 
+imgUp = tk.PhotoImage(file = r"images\upArrow.png").subsample(20, 20)
+imgLf = tk.PhotoImage(file = r"images\leftArrow.png").subsample(20, 20)
+imgRg = tk.PhotoImage(file = r"images\rightArrow.png").subsample(20, 20)
+imgWheel = tk.PhotoImage(file = r"images\wheel.png").subsample(20, 20)
+imgDown = tk.PhotoImage(file = r"images\downArrow.png").subsample(20, 20)
+
+########################################################################
+######################### LEFT TV ######################################
+########################################################################
+
+#Frame Ten TV
+frame1 = tk.Frame(master=window, width=200, height=30, bg="yellow")
+#frame1.pack(fill=tk.BOTH, side=tk.TOP, expand=True)
+#Them label TV LEFT/RIGHT
+llb_TVLeft = tk.Label(master=frame1, text="LG L", bg="yellow")
+llb_TVLeft.pack(fill=tk.BOTH, side=tk.LEFT, expand=True, padx=5, pady=5)
+
+#Frame nut on/off
+frame2 = tk.Frame(master=window, width=200, height=100)
+#frame2.pack(fill=tk.BOTH, side=tk.TOP, expand=True)
+#Them button TV on/off
+lbtON_TVLeft = tk.Button(master=frame2, text="ON", bg="grey")
+lbtON_TVLeft.pack(fill=tk.BOTH, side=tk.LEFT, expand=True, padx=5, pady=5)
+lbtOFF_TVRight = tk.Button(master=frame2, text="OFF", bg="grey")
+lbtOFF_TVRight.pack(fill=tk.BOTH, side=tk.RIGHT, expand=True, padx=5, pady=5)
+
+#Frame nut HDMI
+frame3 = tk.Frame(master=window, width=200, height=100)
+#frame3.pack(fill=tk.BOTH, side=tk.TOP, expand=True)
+lbtHDMI1_TVLeft = tk.Button(master=frame3, text="HDMI_1", bg="grey")
+lbtHDMI2_TVLeft = tk.Button(master=frame3, text="HDMI_2", bg="grey")
+lbtHDMI1_TVLeft.pack(fill=tk.BOTH, side=tk.TOP, expand=True, padx=5, pady=5)
+lbtHDMI2_TVLeft.pack(fill=tk.BOTH, side=tk.BOTTOM, expand=True, padx=5, pady=5)
+
+#Frame tang giam am luong
+frame4 = tk.Frame(master=window, width=200, height=100)
+#frame4.pack(fill=tk.BOTH, side=tk.TOP, expand=True)
+lvol = tk.Scale(
+    frame4,
+    from_ = 0,
+    to = 100,
+    orient = HORIZONTAL,
+	#tickinterval=10,
+    resolution = 1,
+    ####################
+    #command=change_vol
+    ####################
+)
+lvol.pack(fill=tk.BOTH, side=tk.TOP, expand=True, padx=5, pady=5)
+
+#Frame mui ten dieu huong
+frame5 = tk.Frame(master=window, width=200, height=100)
+
+lbtArrowUp = tk.Button(master=frame5, text="", bg="grey", image = imgUp)
+lbtArrowUp.pack(fill=tk.BOTH, side=tk.TOP, expand=True, padx=5, pady=5)
+
+#Frame mui ten dieu huong
+frame6 = tk.Frame(master=window, width=200, height=100)
+#frame6.pack(fill=tk.BOTH, side=tk.TOP, expand=True)
+
+lbtArrowLf = tk.Button(master=frame6, text="", bg="grey", image = imgLf)
+lbtArrowLf.pack(fill=tk.BOTH, side=tk.LEFT, expand=True, padx=5, pady=5)
+
+lbtArrowRg = tk.Button(master=frame6, text="", bg="grey", image = imgRg)
+lbtArrowRg.pack(fill=tk.BOTH, side=tk.RIGHT, expand=True, padx=5, pady=5)
 
 
-search_config = [
-    "/etc/lgtv/config.json",
-    "~/.lgtv/config.json",
-    "/opt/venvs/lgtv/config/config.json"
-]
+lbtWheel = tk.Button(master=frame6, text="", bg="grey", image = imgWheel)
+lbtWheel.pack(fill=tk.BOTH, side=tk.BOTTOM, expand=True, padx=5, pady=5)
 
-def usage(error=None):
-    if error:
-        print ("Error: " + error)
-    print ("LGTV Controller")
-    print ("Author: Karl Lattimer <karl@qdh.org.uk>")
-    print ("Usage: lgtv <command> [parameter]\n")
-    print ("Available Commands:")
+#Frame mui ten dieu huong
+frame7 = tk.Frame(master=window, width=200, height=100)
 
-    print ("  -i                    interactive mode")
+lbtArrowUp = tk.Button(master=frame7, text="", bg="grey", image = imgDown)
+lbtArrowUp.pack(fill=tk.BOTH, side=tk.TOP, expand=True, padx=5, pady=5)
 
-    print ("  scan")
-    print ("  auth <host>")
-
-    commands = LGTVRemote.getCommands()
-    for c in commands:
-        args = getargspec(LGTVRemote.__dict__[c])
-        if len(args.args) > 1:
-            a = ' <' + '> <'.join(args.args[1:-1]) + '>'
-            print ('  ' + c + a)
-        else:
-            print ('  ' + c)
-
-def parseargs(command, argv):
-    args = getargspec(LGTVRemote.__dict__[command])
-    args = args.args[1:-1]
-
-    if len(args) != len(argv):
-        raise Exception("Argument lengths do not match")
-
-    output = {}
-    for (i, a) in enumerate(args):
-        if argv[i].lower() == "true":
-            argv[i] = True
-        elif argv[i].lower() == "false":
-            argv[i] = False
-        try:
-            f = int(argv[i])
-            argv[i] = f
-        except:
-            try:
-                f = float(argv[i])
-                argv[i] = f
-            except:
-                pass
-        output[a] = argv[i]
-    return output
+#Frame nut control
+frame8 = tk.Frame(master=window, width=200, height=100)
+#frame8.pack(fill=tk.BOTH, side=tk.TOP, expand=True)
+lbtBack = tk.Button(master=frame8, text="Back", bg="grey")
+lbtExit = tk.Button(master=frame8, text="Exit", bg="grey")
+lbtBack.pack(fill=tk.BOTH, side=tk.LEFT, expand=True, padx=5, pady=5)
+lbtExit.pack(fill=tk.BOTH, side=tk.RIGHT, expand=True, padx=5, pady=5)
 
 
-def find_config():
-    w = None
-    for f in search_config:
-        f = os.path.expanduser(f)
-        f = os.path.abspath(f)
-        d = os.path.dirname(f)
-        if os.path.exists(d):
-            if os.access(d, os.W_OK):
-                w = f
-            if os.path.exists(f):
-                if os.access(f, os.W_OK):
-                    return f
-        elif os.access(os.path.dirname(d), os.W_OK):
-            os.makedirs(d)
-            w = f
-    if w is None:
-        print ("Cannot find suitable config path to write, create one in %s" % ' or '.join(search_config))
-        raise Exception("No config file")
-    return w
+########################################################################
+######################### RIGHT TV ######################################
+########################################################################
 
-def send_command(name, command, args, config):
-    if command == "on":
-        ws = LGTVRemote(name, **config[name])
-        ws.on()
-        return
-    else:
-        try:
-            args = parseargs(name, args)
-        except Exception as e:
-            usage(e.message)
-            return
-    try:
-        ws = LGTVRemote(name, **config[name])
-        ws.connect()
-        if command is not None:
-            ws.execute(command, args)
-        ws.run_forever()
-    except KeyboardInterrupt:
-        ws.close()
+#Frame Ten TV
+frame9 = tk.Frame(master=window, width=200, height=30, bg="yellow")
+#Them label TV LEFT/RIGHT
+rlb_TVLeft = tk.Label(master=frame9, text="LG R", bg="yellow")
+rlb_TVLeft.pack(fill=tk.BOTH, side=tk.LEFT, expand=True, padx=5, pady=5)
 
-def read_config_file(name, host):
-    data = None
-    config_file = name + "_" + host + ".json"
-    if os.path.isfile(config) == True:
-        with open(file) as f:
-            data = json.load(f)
-    return data
+#Frame nut on/off
+frame10 = tk.Frame(master=window, width=200, height=100)
+#Them button TV on/off
+rbtON_TVLeft = tk.Button(master=frame10, text="ON", bg="grey")
+rbtON_TVLeft.pack(fill=tk.BOTH, side=tk.LEFT, expand=True, padx=5, pady=5)
+rbtOFF_TVRight = tk.Button(master=frame10, text="OFF", bg="grey")
+rbtOFF_TVRight.pack(fill=tk.BOTH, side=tk.RIGHT, expand=True, padx=5, pady=5)
 
-def pair_device(name, host):
-    print("Start to authen device : %s - %s" % (name, host))
-    filename = name + "_" + host + ".json"
-    filename = os.path.join(workspace,filename)
-    ws = LGTVAuth(name, host)
-    ws.connect()
-    ws.run_forever()
-    sleep(1)
-    config = ws.serialise()
-    if filename is not None:
-        with open(filename, 'w') as f:
-            f.write(json.dumps(config))
-        print ("Wrote config file: " + filename)
+#Frame nut HDMI
+frame11 = tk.Frame(master=window, width=200, height=100)
+rbtHDMI1_TVLeft = tk.Button(master=frame11, text="HDMI_1", bg="grey")
+rbtHDMI2_TVLeft = tk.Button(master=frame11, text="HDMI_2", bg="grey")
+rbtHDMI1_TVLeft.pack(fill=tk.BOTH, side=tk.TOP, expand=True, padx=5, pady=5)
+rbtHDMI2_TVLeft.pack(fill=tk.BOTH, side=tk.BOTTOM, expand=True, padx=5, pady=5)
 
-def read_devices_list(file):
-    devices = None
-    if os.path.isfile(file) == True:
-        with open(file) as f:
-            devices = json.load(f)
-    return devices
+#Frame tang giam am luong
+frame12 = tk.Frame(master=window, width=200, height=100)
+rvol = tk.Scale(
+    frame12,
+    from_ = 0,
+    to = 100,
+    orient = HORIZONTAL,
+	#tickinterval=10,
+    resolution = 1,
+    ####################
+    #command=change_vol
+    ####################
+)
+rvol.pack(fill=tk.BOTH, side=tk.TOP, expand=True, padx=5, pady=5)
 
-def scan_devices(output_file):
-    results = LGTVScan()
-    if len(results) > 0:
-        print (json.dumps({
-            "result": "ok",
-            "count": len(results),
-            "list": results
-        }))
-        for it in results:
-            uuid = it['uuid']
-            address = it['address']
-            model = it['model']
-            #Save to json file
-            data = []
-            data.append({
-                'uuid': it['uuid'],
-                'model': it['model'],
-                'address': it['address'],
-            })
-        with open(outfile, 'w') as outfile:
-            json.dump(data, outfile)
-    else:
-        print (json.dumps({
-            "result": "failed",
-            "count": len(results)
-        }))
-        return
+#Frame mui ten dieu huong
+frame13 = tk.Frame(master=window, width=200, height=100)
 
-def main():
-    devices_list = os.path.join(workspace,'devices.json')
-    # Scan all devices
-    scan_devices(devices_list)
+rbtArrowUp = tk.Button(master=frame13, text="", bg="grey", image = imgUp)
+rbtArrowUp.pack(fill=tk.BOTH, side=tk.TOP, expand=True, padx=5, pady=5)
 
-    # Read devices list
-    devices = read_devices_list(devices_list)
+#Frame mui ten dieu huong
+frame14 = tk.Frame(master=window, width=200, height=100)
+rbtArrowLf = tk.Button(master=frame14, text="", bg="grey", image = imgLf)
+rbtArrowLf.pack(fill=tk.BOTH, side=tk.LEFT, expand=True, padx=5, pady=5)
 
-    # Pair devices
-    for item in devices:
-        pair_device(item['uuid'], item['address'])
+rbtArrowRg = tk.Button(master=frame14, text="", bg="grey", image = imgRg)
+rbtArrowRg.pack(fill=tk.BOTH, side=tk.RIGHT, expand=True, padx=5, pady=5)
 
-    # Send command
-    for item in devices:
-        config =  read_config_file(item['uuid'], item['address'])
-        name = item['uuid']
-        command = "off"
-        args = []
-        send_command(name, command, args, data)
-        
-if __name__ == '__main__':
-    workspace = os.path.join(os.path.expanduser("~"),".lgtv")
-    if not os.path.exists(workspace):
-        os.makedirs(workspace)
-    print("workspace : %s" % workspace)
-    # main()
-    print("Bye")
-    input("Press Enter to continue...")
+
+rbtWheel = tk.Button(master=frame14, text="", bg="grey", image = imgWheel)
+rbtWheel.pack(fill=tk.BOTH, side=tk.BOTTOM, expand=True, padx=5, pady=5)
+
+#Frame mui ten dieu huong
+frame15 = tk.Frame(master=window, width=200, height=100)
+
+lbtArrowUp = tk.Button(master=frame15, text="", bg="grey", image = imgDown)
+lbtArrowUp.pack(fill=tk.BOTH, side=tk.TOP, expand=True, padx=5, pady=5)
+
+#Frame nut control
+frame16 = tk.Frame(master=window, width=200, height=100)
+rbtBack = tk.Button(master=frame16, text="Back", bg="grey")
+rbtExit = tk.Button(master=frame16, text="Exit", bg="grey")
+rbtBack.pack(fill=tk.BOTH, side=tk.LEFT, expand=True, padx=5, pady=5)
+rbtExit.pack(fill=tk.BOTH, side=tk.RIGHT, expand=True, padx=5, pady=5)
+
+############################
+# Aligment Frame
+############################
+frame1.grid(row=0, column=0)
+frame2.grid(row=1, column=0)
+frame3.grid(row=2, column=0)
+frame4.grid(row=3, column=0)
+frame5.grid(row=4, column=0)
+frame6.grid(row=5, column=0)
+frame7.grid(row=6, column=0)
+frame8.grid(row=7, column=0)
+
+frame0 = tk.Frame(master=window, width=50, height=900)
+label = tk.Label(
+	master=frame0,
+    text="",
+    foreground="white",  # Set the text color to white
+    background="black"  # Set the background color to black
+)
+label.pack(fill=tk.BOTH, expand=True)
+
+frame0.grid(row=2, column=1)
+
+
+frame9.grid(row=0, column=2)
+frame10.grid(row=1, column=2)
+frame11.grid(row=2, column=2)
+frame12.grid(row=3, column=2)
+frame13.grid(row=4, column=2)
+frame14.grid(row=5, column=2)
+frame15.grid(row=6, column=2)
+frame16.grid(row=7, column=2)
+
+#################
+#Start gui
+#################
+window.resizable(0, 0)
+window.eval('tk::PlaceWindow . center')
+window.mainloop()
